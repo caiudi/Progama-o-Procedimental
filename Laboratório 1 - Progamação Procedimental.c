c52e905840213e2447904fedc15df4dea5ed8f49
caiudi <127546034+caiudi@users.noreply.github.com>
2023-03-10 20:48:31 -0300

    Update Laboratório 1 - Progamação Procedimental.c

--- Laboratório 1 - Progamação Procedimental.c
+++ Laboratório 1 - Progamação Procedimental.c
@@ -1,3 +1,5 @@
+//Exercício 1
+
 #include <stdio.h> 
 int main(){
     int x;
@@ -5,3 +7,13 @@ int main(){
     printf("Valor de x: %d", x);
     return 0;
 }
+
+//Exercício 2
+
+#include <stdio.h> 
+int main(){
+    int x;
+    scanf("%f", &x);
+    printf("Valor de x: %f", x);
+    return 0;
+    
