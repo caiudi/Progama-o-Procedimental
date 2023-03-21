04fb2d948020476b82f7e5736b99911262831e16
caiudi <127546034+caiudi@users.noreply.github.com>
2023-03-21 18:01:20 -0300

    Update Laboratório 1 - Progamação Procedimental.c

--- Laboratório 1 - Progamação Procedimental.c
+++ Laboratório 1 - Progamação Procedimental.c
@@ -631,6 +631,22 @@ main()
 
 //Exercício 51
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int 
+main()
+{
+   float d, x, y;
+   printf ("Digite o valor de x e y");
+   scanf ("%f %f", &x, &y);
+   d = sqrt(pow(x,2)+pow(y,2));
+   printf ("A dista2]ncia e = %f", d);
+   return 0;
+}
+
+//Exercício 52
+
 
 
 
