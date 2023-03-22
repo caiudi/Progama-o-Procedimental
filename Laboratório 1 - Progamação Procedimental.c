f90db60d12e45c6385ae5d708828fd66344241e4
caiudi <127546034+caiudi@users.noreply.github.com>
2023-03-22 15:15:54 -0300

    Update Laboratório 1 - Progamação Procedimental.c

--- Laboratório 1 - Progamação Procedimental.c
+++ Laboratório 1 - Progamação Procedimental.c
@@ -647,6 +647,33 @@ main()
 
 //Exercício 52
 
+#include <stdio.h>
+#include <stdlib.h>
+int 
+main()
+{
+   float inv1, inv2, inv3, invt, premiot, premio1, premio2, premio3, x, y, z;
+   printf ("Digite o investimento do primeiro apostador");
+   scanf ("%f", &inv1);
+   printf ("Digite o investimento do segundo apostador");
+   scanf ("%f", &inv2);
+   printf ("Digite o investimento do terceiro apostador");
+   scanf ("%f", &inv3);
+   printf ("Digite o valor do premio");
+   scanf ("%f", &premiot);
+   invt = inv1 + inv2 + inv3;
+   x = inv1/invt;
+   y = inv2/invt;
+   z = inv3/invt;
+   premio1 = x*premiot;
+   premio2 = y*premiot;
+   premio3 = z*premiot;
+   printf ("%f %f %f", premio1, premio2, premio3);
+ }
+
+//Exercício 53
+
+
 
 
 
