0672e9de1923cc2ddc5614d62ed18c972627f9f0
caiudi <127546034+caiudi@users.noreply.github.com>
2023-03-22 15:27:07 -0300

    Update Laboratório 1 - Progamação Procedimental.c

--- Laboratório 1 - Progamação Procedimental.c
+++ Laboratório 1 - Progamação Procedimental.c
@@ -669,11 +669,28 @@ main()
    premio2 = y*premiot;
    premio3 = z*premiot;
    printf ("%f %f %f", premio1, premio2, premio3);
+   return 0;
  }
+
 
 //Exercício 53
 
-
+#include <stdio.h>
+#include <stdlib.h>
+int 
+main()
+{
+   float l, c, tela, p;
+   printf ("Digite a largura do terreno");
+   scanf ("%f", &l);
+   printf ("Digite o comprimento do terreno");
+   scanf ("%f", &c);
+   printf ("Digite o preço do metro de tela");
+   scanf ("%f", &p);
+   tela = l*c*p;
+   printf ("&f", tela);
+   return 0;
+}
 
 
 
