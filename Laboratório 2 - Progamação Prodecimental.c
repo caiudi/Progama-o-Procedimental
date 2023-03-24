52e7c4b9af4b9653ebcef02676f8ed18de460eb4
caiudi <127546034+caiudi@users.noreply.github.com>
2023-03-24 09:41:08 -0300

    Update Laboratório 2 - Progamação Prodecimental.c

--- Laboratório 2 - Progamação Prodecimental.c
+++ Laboratório 2 - Progamação Prodecimental.c
@@ -6,13 +6,17 @@ int
 main()
 { 
   int x, y;
-
+  
   printf ("Digite o primeiro numero:");
   scanf ("%i", &x);
+  
   printf ("\n");
+  
   printf ("Digite o segundo numero");
   scanf ("%i", &y);
+  
   printf("\n");
+  
   if(x>y){ 
   printf ("O primeiro numero e maior que o segundo");
     return 0;
@@ -23,5 +27,31 @@ main()
   }
   
   //Exercício 2
+  
+  #include <stdio.h>
+#include <math.h>
+int
+main()
+{ 
+  float x, raiz;
+  
+  printf ("Digite um numero real");
+  scanf ("%f", &x);
+  
+  printf ("\n");
+  
+  if(x>=0)
+  {
+    raiz = sqrt(x);
+    printf("A raiz do numero e %.2f", raiz);
+    return 0;
+  }
+  else
+  {
+    printf("O numero e invalido");
+    return 0;
+  }
+
+  //Exercício 3
   
   
