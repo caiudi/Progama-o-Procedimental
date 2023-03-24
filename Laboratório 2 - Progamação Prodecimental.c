6fc939689c62ac4879ae03b25c04d1161cef6023
caiudi <127546034+caiudi@users.noreply.github.com>
2023-03-24 09:31:35 -0300

    Update and rename Laboratório 2 - Progamação Prodecimental to Laboratório 2 - Progamação Prodecimental.c

---
+++ Laboratório 2 - Progamação Prodecimental.c
@@ -0,0 +1,27 @@
+//Exercício 1
+
+#include <stdio.h>
+
+int
+main()
+{ 
+  int x, y;
+
+  printf ("Digite o primeiro numero:");
+  scanf ("%i", &x);
+  printf ("\n");
+  printf ("Digite o segundo numero");
+  scanf ("%i", &y);
+  printf("\n");
+  if(x>y){ 
+  printf ("O primeiro numero e maior que o segundo");
+    return 0;
+  }
+  else{
+    printf ("O segundo numero e maior que o primeiro");
+    return 0; 
+  }
+  
+  //Exercício 2
+  
+  
