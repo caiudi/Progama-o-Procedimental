70738ef924b41b0ff40faec2fb55d33a486904eb
caiudi <127546034+caiudi@users.noreply.github.com>
2023-03-26 11:28:49 -0300

    Update Laboratório 2 - Progamação Prodecimental.c

--- Laboratório 2 - Progamação Prodecimental.c
+++ Laboratório 2 - Progamação Prodecimental.c
@@ -511,7 +511,51 @@ main()
   
 //Exercicio 16
   
+#include <stdio.h>
+#include <math.h>
+int 
+main()
+{
+    int numero;
+
+    printf ("Digite um numero inteiro [0 a 12]\n");
+    scanf ("%i", &numero);
+
+    printf ("\n");
+
+    switch ( numero )
+    {
+        case 1:
+            printf ("Janeiro\n"); break;
+        case 2:
+            printf ("Fevereiro\n"); break;
+        case 3:
+            printf ("Marco\n"); break;
+        case 4:
+            printf ("Abril\n"); break;
+        case 5:
+            printf ("Maio\n"); break;
+        case 6:
+            printf ("Junho\n"); break;
+        case 7:
+            printf ("Julho\n"); break;
+        case 8:
+            printf ("Agosto\n"); break;
+        case 9:
+            printf ("Setembro\n"); break;
+        case 10:
+            printf ("Outubro\n"); break;
+        case 11:
+            printf ("Novembro\n"); break;
+        case 12:
+            printf ("Dezembro\n"); break;
+        default:
+            printf ("Nao corresponde a um mes"); break;
+    } 
+    return 0;
+}
   
+//Exercício 17
 
 
   
