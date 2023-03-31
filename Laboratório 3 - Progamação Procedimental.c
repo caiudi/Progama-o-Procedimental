ba998fef23026553247ef2603086ab6cb64c6ad4
caiudi <127546034+caiudi@users.noreply.github.com>
2023-03-31 09:36:50 -0300

    Update and rename Laboratório 3 - Progamação Procedimental to Laboratório 3 - Progamação Procedimental.c

---
+++ Laboratório 3 - Progamação Procedimental.c
@@ -0,0 +1,22 @@
+//Exercicio 1
+
+#include <stdio.h>
+int
+main()
+{
+    int x=0, y=0;
+    do
+    {
+        y = y+3;
+        x++;
+        printf ("%i\n", y);
+    } 
+    while (x<5);
+    return 0;
+}
+
+//Exercicio 2
+
+
+
+
