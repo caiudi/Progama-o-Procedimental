0e374a8dea816ae30548e16286ea1358ab255b52
caiudi <127546034+caiudi@users.noreply.github.com>
2023-03-31 09:46:55 -0300

    Update Laboratório 3 - Progamação Procedimental.c

--- Laboratório 3 - Progamação Procedimental.c
+++ Laboratório 3 - Progamação Procedimental.c
@@ -17,6 +17,29 @@ main()
 
 //Exercicio 2
 
+#include <stdio.h>
+int
+main()
+{
+    int x, y=1, z=1;
+    for (x=1 ; x <=100 ; x++)
+    {
+        printf ("%i\n", x);
+    }
+    while ( y <=100)
+    {
+    printf ("%i\n", y);
+    y++;
+    }
+    do
+    {
+        printf ("%i\n", z);
+        z++;
+    } while ( z<=100);
+}
+
+//Exercicio 3
+
 
 
 
