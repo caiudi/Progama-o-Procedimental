76f80abaf005a48af6326a02be44f8c87bfb526e
caiudi <127546034+caiudi@users.noreply.github.com>
2023-04-14 09:38:30 -0300

    Update Laboratório 4 - Progamação Procedimental.c

--- Laboratório 4 - Progamação Procedimental.c
+++ Laboratório 4 - Progamação Procedimental.c
@@ -1,1 +1,21 @@
+//Exercício 1
+#include <stdio.h>
+
+int dobro(int x)
+{
+    return 2*x;
+}
+
+int 
+main()
+{
+    int x, y;
+    printf ("Escreva um numero\n");
+    scanf ("%i", &x);
+    y = dobro(x);
+    printf ("%i", y);
+    return 0;
+}
+
+//Exercício 2
 
