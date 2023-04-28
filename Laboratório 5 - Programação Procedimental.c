b4a010ef9176fed6730c9ac65deb42c123d2ef01
caiudi <127546034+caiudi@users.noreply.github.com>
2023-04-28 09:56:45 -0300

    Update Laboratório 5 - Programação Procedimental.c

--- Laboratório 5 - Programação Procedimental.c
+++ Laboratório 5 - Programação Procedimental.c
@@ -118,3 +118,21 @@ main()
 }
 
 //Exercício 5
+
+#include <stdio.h>
+
+int
+main()
+{
+    int vet[50];
+    int i;
+
+    for (i = 0 ; i < 50 ; i++)
+    {
+        vet[i] = (i+5*i)%(i+1);
+        printf ("%i\n", vet[i]);
+    }
+    return 0;
+}
+
+//Exercício 6
