5678fe5749ce8fbda04574d35250ef1eb9dd3e48
caiudi <127546034+caiudi@users.noreply.github.com>
2023-05-07 16:35:29 -0300

    Update Laboratório 6 - Progamação Procedimental.c

--- Laboratório 6 - Progamação Procedimental.c
+++ Laboratório 6 - Progamação Procedimental.c
@@ -1,1 +1,29 @@
+//Exercício 1
+
+#include <stdio.h>
+
+struct Horário
+{
+  int hora;
+  int minuto;
+  int segundos;
+};
+
+struct Data
+{
+  int dia;
+  int mes;
+  int ano;
+};
+
+struct Compromisso
+{
+ 
+  int data;
+  int horario;
+  char texto[100];
+};
+
+//Exercício 2
+
 
